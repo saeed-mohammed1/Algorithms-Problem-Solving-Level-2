a133a142670c5b103e8f92e8c7364fa1e8f4d3ff
Saeed Mohammed <saeedmohammed000111@gmail.com>
2026-08-25 11:50:24 +0300

    Rename function to count digit frequency

---
+++ Problem_08.cpp
@@ -0,0 +1,48 @@
+/*
+  =============================================================================
+  Problem #8 (Level 2):
+  Write a program to ask the user to enter a positive number and a specific
+  digit, then count and print the frequency of that digit within the number.
+  =============================================================================
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int ReadPositiveNumber(string message)
+{
+	int Number;
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+
+int CountDigitFrequency(int Number , int Digit)
+{
+	int Reminder = 0, Counter = 0;
+	
+	while (Number > 0)
+	{
+		Reminder = Number % 10;
+		Number = Number / 10;
+		if (Reminder == Digit)
+			Counter++;
+	}
+	
+	return Counter;
+}
+
+
+int main()
+{
+	int Number = ReadPositiveNumber("Please enter the main number?");
+	int Digit = ReadPositiveNumber("Please enter one digit to check?");
+	cout << "Digit " << Digit << " Frequency is " << CountDigitFrequency(Number, Digit) << " Time(s).";
+	
+	return 0;
+}
