30b27ee686a9d279f9af13f1b4ade1ce0c3dd9bf
Saeed Mohammed <saeedmohammed000111@gmail.com>
2026-09-02 06:18:44 +0300

    Create Problem_25.cpp
    Added a comment block describing the problem statement for Problem #25.

---
+++ Problem_25.cpp
@@ -0,0 +1,67 @@
+/*
+  =============================================================================
+  Problem #25 (Level 2):
+  Write a program to fill an array of size N with random numbers in the range
+  [1 to 100], print the array, then find and print the minimum number in it.
+  =============================================================================
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
+
+void ReadArray(int arr[100], int& arrLength)
+{
+	cout << "enter length of array? \n";
+	cin >> arrLength;
+
+	for (int i = 0; i < arrLength; i++)
+	{
+		
+		arr[i] = RandomNumber(1 , 100);
+
+	}
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int MinNumberInArray(int arr[100], int arrLength)
+{
+	int Min = 0;
+	Min = arr[0];
+	for (int i = 0; i < arrLength; i++)
+	{
+		if (Min > arr[i])
+		{
+			Min = arr[i];
+		}
+	}
+	return Min;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[100], arrLength;
+
+	ReadArray(arr, arrLength);
+	cout << "\nArray Elements: ";
+	PrintArray(arr, arrLength);
+	cout << "\nMin Number is ";
+	cout << MinNumberInArray(arr, arrLength) << endl;
+	return 0;
+}
