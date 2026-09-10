19a9c8d8e72530751c154070560600d5da0cc29e
Saeed Mohammed <saeedmohammed000111@gmail.com>
2026-09-10 08:43:17 +0300

    Implement random number array generation and copying
    The program generates an array of random numbers, copies it to another array, and prints both arrays.

---
+++ Problem_37.cpp
@@ -0,0 +1,84 @@
+/*
+  =============================================================================
+  Problem #37 (Level 2):
+  Write a program to fill an array of max size 100 with random numbers from
+  1 to 100, copy its elements to another array using the AddArrayElement function,
+  and print both arrays.
+  =============================================================================
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
+
+int ReadPositiveNumber(string message)
+{
+	int Number;
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+void AddArrayElements(int Number, int arr[100], int& arrLength)
+{
+	arrLength++;
+	arr[arrLength - 1] = Number;
+}
+
+void ReadArray(int arr[100], int arrLength)
+{
+
+	for (int i = 0; i < arrLength; i++)
+	{
+
+		arr[i] = RandomNumber(1, 100);
+
+	}
+}
+
+void CopyArrayElements(int arr[100], int arr2[100], int arrLength, int &Length2)
+{
+	
+	for (int i = 0; i < arrLength; i++)
+	{
+		AddArrayElements(arr[i], arr2, Length2);
+	}
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[100], arrLength = 0, arrCopy[100], Length2 = 0;
+	arrLength = ReadPositiveNumber("Please enter your Length?");
+	ReadArray(arr, arrLength);
+
+	CopyArrayElements(arr, arrCopy, arrLength, Length2);
+	cout << "\nArray 1 elements :\n";
+	PrintArray(arr, arrLength);
+
+	cout << "\nArray 2 elements after copy:\n";
+	PrintArray(arrCopy, Length2);
+
+	return 0;
+}
